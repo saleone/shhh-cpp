c452012d705c8f1c9747b9d5f11a4efa59dc196c
Saša Savić <sasa.savic@pm.me>
2018-12-30 09:06:18 -0500

    Notify all subs

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: all run clean
+.PHONY: all run clean debug
 
 all: clean build
 
@@ -6,12 +6,14 @@ all: clean build
 build:
 	mkdir build
 	cd build
-	clang -flto -O3 -c -I../src/vendor/uWebSockets/uSockets/src ../src/vendor/uWebSockets/uSockets/src/*.c ../src/vendor/uWebSockets/uSockets/src/eventing/*.c
-	clang++ -flto -O3 -c -std=c++17 -I../src/vendor/uWebSockets/src -I../src/vendor/uWebSockets/uSockets/src ../src/main.cpp
+	clang -flto -O3 -c -I../src/vendor/uWebSockets/uSockets/src ../src/vendor/uWebSockets/uSockets/src/*.c ../src/vendor/uWebSockets/uSockets/src/eventing/*.c &&\
+	clang++ -flto -O3 -c -std=c++17 -I../src/vendor/uWebSockets/src -I../src/vendor/uWebSockets/uSockets/src ../src/main.cpp &&\
 	clang++ -flto -O3 -s *.o -o ./program -lz
 
 run:
 	./build/program
+
+debug: clean build run
 
 clean:
 	rm -fr ./build
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
+#include <algorithm>
 #include <iostream>
 #include <stdexcept>
+#include <string>
 #include <string_view>
 #include <cstdint>
+#include <forward_list>
+#include <iterator>
 
 #include "vendor/uWebSockets/src/App.h"
 
 
 #define DEFAULT_PORT 9876
+#define SSL false
 
+typedef uWS::TemplatedApp<SSL> App;
 
 uint16_t port = DEFAULT_PORT;
 struct PerSocketData {};
 
+static bool lock = false;
+static std::forward_list<uWS::WebSocket<SSL,true> *> clients;
+
 uint16_t get_port(char *);
+void notify_all(std::forward_list<uWS::WebSocket<SSL,true> *>, std::string);
+void notify_all(std::forward_list<uWS::WebSocket<SSL,true> *>, bool);
 
 int main(int argc, char **argv)
 {
@@ -22,28 +33,48 @@ int main(int argc, char **argv)
     if (port == 0)
         return 1;
 
-    uWS::App().ws<PerSocketData>("/*", {
+    App().ws<PerSocketData>("/*", {
         .compression = uWS::SHARED_COMPRESSOR,
         .maxPayloadLength = 16 * 1024,
-        .open = [](auto *ws, auto *req) {},
+        .open = [](auto *ws, auto *req) {
+            clients.push_front(ws);
+            std::cout << "+ connection -> " << std::distance(clients.begin(), clients.end()) << std::endl;
+        },
         .message = [](auto *ws, std::string_view message, uWS::OpCode opCode) {
-            ws->send(message, opCode);
+            lock = !lock;
+            notify_all(clients, lock);
         },
         .drain = [](auto *ws) {},
         .ping = [](auto *ws) {},
         .pong = [](auto *ws) {},
-        .close = [](auto *ws, int code, std::string_view message) {}
+        .close = [](auto *ws, int code, std::string_view message) {
+            clients.remove(ws);
+            std::cout << "- connection -> " << std::distance(clients.begin(), clients.end()) << std::endl;
+        }
     }).listen(port, [](auto *token) {
         if (token)
-            std::cout << "Listening on port " << port << std::endl;
+            std::cout << "Running: " << port << std::endl;
     }).run();
 
     return 0;
 }
 
+void notify_all(std::forward_list<uWS::WebSocket<SSL,true> *> conns, bool lock)
+{
+    notify_all(conns, lock ? "LOCKED" : "UNLOCKED");
+}
+
+void notify_all(std::forward_list<uWS::WebSocket<SSL,true> *> conns, std::string msg)
+{
+    std::for_each(conns.begin(), conns.end(), [msg](auto *ws) {
+        ws->send(msg, uWS::OpCode::TEXT);
+    });
+}
+
 uint16_t get_port(char *user_port)
 {
-    try {
+    try
+    {
         int passed_port = std::stoi(user_port);
 
         if (passed_port > UINT16_MAX)
